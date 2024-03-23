e800410d3f37bc58e7bfe0e3abb2acbcc616bbf5
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-03-23 23:59:10 +0100

    feat(server): Add projects & settings dbs and api get routes

---
+++ composables/useProjectsDb.ts
@@ -0,0 +1,31 @@
+/*
+ * File: useProjectsDb.ts
+ * Project: git-anon
+ * Created Date: 2024-03-23 22:34:23
+ * Author: 3urobeat
+ *
+ * Last Modified: 2024-03-23 23:56:50
+ * Modified By: 3urobeat
+ *
+ * Copyright (c) 2024 3urobeat <https://github.com/3urobeat>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+import nedb from "@seald-io/nedb";
+
+
+// Load database
+const projectsDb = new nedb({ filename: "data/database/projects.db", autoload: true });
+
+
+/**
+ * Provide function to access Database instance from API
+ * @returns Nedb database instance
+ */
+export function useProjectsDb() {
+    return projectsDb;
+}
---
+++ server/api/get-projects.ts
@@ -0,0 +1,42 @@
+/*
+ * File: get-projects.ts
+ * Project: git-anon
+ * Created Date: 2024-03-23 21:28:59
+ * Author: 3urobeat
+ *
+ * Last Modified: 2024-03-23 23:55:00
+ * Modified By: 3urobeat
+ *
+ * Copyright (c) 2024 3urobeat <https://github.com/3urobeat>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+import { useProjectsDb } from "../../composables/useProjectsDb";
+
+
+/**
+ * This API route gets all projects and their details and returns them
+ * Params: {}
+ * Returns: "{ name: string, details: { name: string }[] }[]"
+ */
+
+
+// This function is executed when this API route is called
+export default defineEventHandler(async () => {
+
+    // Get database instance
+    const db = useProjectsDb();
+
+    console.log("API get-projects: Received get-projects request");
+
+
+    // Get all projects and their details
+    const data = await db.findAsync({});
+
+    return data;
+
+});
