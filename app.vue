66f0cff96cfc147bcc11a85ebc7ffb9a5ca5979b
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-03-24 13:11:09 +0100

    styling: Add icons and hover color + transition to navbar

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
  * Created Date: 2024-03-23 12:52:57
  * Author: 3urobeat
  *
- * Last Modified: 2024-03-23 20:59:04
+ * Last Modified: 2024-03-24 13:08:57
  * Modified By: 3urobeat
  *
  * Copyright (c) 2024 3urobeat <https://github.com/3urobeat>
@@ -37,10 +37,19 @@
         <nav id="navbar" class="absolute hidden md:block h-full md:w-1/6 w-1/2 bg-white border-2 border-x-gray-500 border-l-0">
             <div class="flex flex-col px-5 md:px-10">                          <!-- px-10 gives every element in this div space on the sides -->
                 <div class="my-2"></div>                                       <!-- Add some space above everything-->
-                <button class="my-1">Home</button>
+                <button class="flex items-center justify-center my-1 rounded-sm hover:bg-gray-200 hover:transition-all">
+                    <PhHouse class="mr-2"></PhHouse>
+                    Home
+                </button>
                 <div class="my-2 w-full h-0.5 bg-gray-500 opacity-50"></div> <!-- Divider to give Home more presence -->
-                <button class="my-1">Projects</button>
-                <button class="my-1">Settings</button>
+                <button class="flex items-center justify-center my-1 rounded-sm hover:bg-gray-200 hover:transition-all">
+                    <PhGitFork class="mr-2"></PhGitFork>
+                    Projects
+                </button>
+                <button class="flex items-center justify-center my-1 rounded-sm hover:bg-gray-200 hover:transition-all">
+                    <PhGear class="mr-2"></PhGear>
+                    Settings
+                </button>
             </div>
         </nav>
 
@@ -53,7 +62,7 @@
         <footer class="self-center flex flex-col items-center mb-2 mt-2 text-xs md:text-sm opacity-50">
             git-anon v{{ packagejson.version }}
 
-            <button type="button" @click="clickGithub" class="mt-1 rounded-full px-2 text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 text-center flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 bg-opacity-80">
+            <button type="button" @click="clickGithub" class="mt-1 rounded-full px-2 text-white bg-[#24292F] hover:bg-[#24292F]/90 hover:transition-all focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 text-center flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 bg-opacity-80">
                 <svg class="mr-1" width="1em" height="1em" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                     <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
                 </svg>
@@ -69,7 +78,7 @@
 
 
 <script setup lang="ts">
-    import { PhList } from "@phosphor-icons/vue";
+    import { PhList, PhHouse, PhGitFork, PhGear } from "@phosphor-icons/vue";
     import packagejson from "./package.json";
 
     /**
