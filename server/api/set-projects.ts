523d33358f58a8f10bc4d577acbfc80b740fd5e5
3urobeat <35304405+3urobeat@users.noreply.github.com>
2024-03-28 21:47:15 +0100

    feat: Add set-projects API route and move data validation from get-projects

---
+++ server/api/set-projects.ts
@@ -0,0 +1,77 @@
+/*
+ * File: set-projects.ts
+ * Project: git-anon
+ * Created Date: 2024-03-28 20:11:07
+ * Author: 3urobeat
+ *
+ * Last Modified: 2024-03-28 21:45:45
+ * Modified By: 3urobeat
+ *
+ * Copyright (c) 2024 3urobeat <https://github.com/3urobeat>
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+import { StoredProjects } from "~/model/projects";
+import { useProjectsDb } from "../../composables/useProjectsDb";
+
+
+/**
+ * This API route saves all projects and their details
+ * Params: { name: string, details: { name: string }[] }[]
+ * Returns: null
+ */
+
+
+// This function is executed when this API route is called
+export default defineEventHandler(async (event) => {
+
+    // Get database instance
+    const db = useProjectsDb();
+
+
+    // Read body of the request we received
+    const params: StoredProjects = await readBody(event);
+
+    if (!params) return false;
+
+    console.log("API set-projects: Received set-projects request");
+
+
+    // Make sure every project has "Commit Message" as first field
+    params.forEach((e, i) => {
+        // Sort to get Commit Message as the first field
+        const sorted = [
+            ...e.details.filter((detail) => detail.name === "Commit Message"),
+            ...e.details.filter((detail) => detail.name !== "Commit Message")
+        ];
+
+        // Overwrite details in original data array
+        params[i].details = sorted;
+
+        // Insert Commit Message field at first index if it does not exist
+        if (params[i].details[0].name !== "Commit Message") {
+            console.log("API set-projects: Project is missing Commit Message field! Inserting...");
+
+            params[i].details.splice(0, 0, { name: "Commit Message", value: "" });
+        }
+    });
+
+
+    // Save changes to database
+    const promises: Promise<{ numAffected: number; affectedDocuments: null; upsert: boolean; }>[] = [];
+
+    params.forEach((e) => {
+        promises.push(db.updateAsync({ name: e.name }, e, { upsert: true }));
+    });
+
+    await Promise.allSettled(promises);
+
+
+    // Indicate we're done
+    return null;
+
+});
